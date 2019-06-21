326cd2c0d5d65a296396bc052dc52b1f3941d07d
Gabe Lopez <glopez11193@gmail.com>
2019-06-21 15:11:09 -0500

    inventory
    inventory added
    trying github pages

---
+++ Pages/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace LopezBlazorWebsite.Pages
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Series { get; set; }
+        public string Color { get; set; }
+        public string VIN { get; set; }
+        public string DriveType { get; set; }
+        public string VehicleType { get; set; }
+        public bool IsSalvage { get; set; }
+        public string FuelType { get; set; }
+        public string Displacement { get; set; }
+        public int? NumDoors { get; set; }
+        public int? NumCylinders { get; set; }
+        public decimal Price { get; set; }
+
+        public string Path { get { return "./CarImages/" + VIN + ".jpg"; } }
+        public string Name { get { return string.Format("{0} {1} {2}",Year, Make, Model); } }
+    }
+}
