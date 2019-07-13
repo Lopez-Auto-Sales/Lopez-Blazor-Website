0c425d62f9bdc9cf54d6f4a464f337294c9c3262
Gabe Lopez <glopez11193@gmail.com>
2019-07-13 17:47:01 -0500

    More detailed car reports

--- Shared/Car.cs
+++ Shared/Car.cs
@@ -1,22 +1,18 @@
-﻿namespace LopezBlazorWebsite.Shared
+﻿using System.Collections.Generic;
+
+namespace LopezBlazorWebsite.Shared
 {
     public class Car
     {
         public int Year { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
-        public string Series { get; set; }
         public string Color { get; set; }
         public string VIN { get; set; }
-        public string DriveType { get; set; }
-        public string VehicleType { get; set; }
         public bool IsSalvage { get; set; }
-        public string FuelType { get; set; }
-        public string Displacement { get; set; }
-        public int? NumDoors { get; set; }
-        public int? NumCylinders { get; set; }
         public int? Mileage { get; set; }
         public decimal Price { get; set; }
+        public IList<JSONExtra> Extras { get; set; }
         public string Path { get { return Constants.imgPrepend + VIN + Constants.imgAppend; } }
         public string Name { get { return string.Format("{0} {1} {2}",Year, Make, Model); } }
         public override string ToString()
@@ -24,5 +20,18 @@
             return VIN;
         }
         public string MileageString { get { return Mileage == null ? "Exempt" : Mileage.Value.ToString("N0"); } }
+    }
+
+    public class JSONExtra
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            if (Name == "Note")
+                return Value;
+            return Name + ": " + Value;
+        }
     }
 }
--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -10,7 +10,7 @@
 
     internal static class Constants
     {
-        internal const string JsonPath = "https://raw.githubusercontent.com/lopezautosales/LopezCars/master/Data/cars.json";
+        internal const string JsonPath = "https://raw.githubusercontent.com/lopezautosales/Lopez-Cars/master/CarData/cars.json";
         internal const string imgPrepend = "https://github.com/lopezautosales/LopezCars/blob/master/CarImages/";
         internal const string imgAppend = ".jpg?raw=true";
     }
